b341954a7016bf0ee8f686193aeb399eb245d0d4
Aubrey Manson <aubreymanson1987@gmail.com>
2019-03-06 17:39:49 -0800

    more changes

--- js/custom.js
+++ js/custom.js
@@ -102,3 +102,47 @@ $(".carousel").carousel();
     mode: "fade"
   });
 })(jQuery);
+
+// portfolio
+if ($(".isotopeWrapper").length) {
+  var $container = $(".isotopeWrapper");
+  var $resize = $(".isotopeWrapper").attr("id");
+  // initialize isotope
+
+  $container.isotope({
+    itemSelector: ".isotopeItem",
+    resizable: false, // disable normal resizing
+    masonry: {
+      columnWidth: $container.width() / $resize
+    }
+  });
+
+  $("#filter a").click(function() {
+    $("#filter a").removeClass("current");
+    $(this).addClass("current");
+    var selector = $(this).attr("data-filter");
+    $container.isotope({
+      filter: selector,
+      animationOptions: {
+        duration: 1000,
+        easing: "easeOutQuart",
+        queue: false
+      }
+    });
+    return false;
+  });
+
+  $(window).smartresize(function() {
+    $container.isotope({
+      // update columnWidth to a percentage of container width
+      masonry: {
+        columnWidth: $container.width() / $resize
+      }
+    });
+  });
+}
+
+$(".navbar-collapse a").on("click", function() {
+  $(".navbar-collapse").removeClass("in");
+  $(".navbar-collapse").addClass("collapse");
+});
